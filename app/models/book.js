36d7ee32f0758546e543174c53793e44cddb7153
Philippe Recto <precto1285@gmal.com>
2018-06-28 08:29:44 -0700

    added book.js

---
+++ app/models/book.js
@@ -0,0 +1,27 @@
+//Dependencies
+var Sequelize = require("sequelize");
+var sequelize = require("../config/connection.js");
+
+//Creates a "Book" model that matches up with DB
+var Book = squelize.define("book", {
+    title:{
+        type: Sequelize.STRING
+    },
+    author: {
+        type: Sequelize.STRING
+    },
+    genre: {
+        type: Sequelize.STRING
+    },
+    pages: {
+        type: Sequelize.INTERGER
+    },
+    timestamps: false
+});
+
+//Syngs DB
+Book.sync();
+
+//Makes the Book Model available
+
+module.exports = Book;
